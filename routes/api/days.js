73437c5945f0d337e9c85c6e235e593379c5fc90
Anne Zhou <anne.zhou920@gmail.com>
2017-02-01 18:33:03 -0500

    api step

---
+++ routes/api/days.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+// var models = require('../../models');
+var Hotel = require('../../models/hotel');
+var Restaurant = require('../../models/restaurant');
+var Activity = require('../../models/activity');
+var Place = require('../../models/place');
+var Day = require('../../models/day');
+
+module.exports = router;
+
+router.post('/days', function(req, res, next){
+  
+})
